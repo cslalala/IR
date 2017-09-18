1e262d9324c915e731dbc1e7851c5f7d7c3a1d6c
apple <513190048@qq.com>
2017-09-18 23:57:48 +0800

    返回注解不加的话就是返回页面

--- src/main/java/ssm/controller/UserController.java
+++ src/main/java/ssm/controller/UserController.java
@@ -1,7 +1,10 @@
 package ssm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ssm.service.UserLogin;
 
 import javax.annotation.Resource;
@@ -13,17 +16,19 @@ import javax.annotation.Resource;
 public class UserController extends BaseController {
     @Resource
     private UserLogin userLogin;
-    public void setUserLogin(UserLogin userLogin){
+
+    public void setUserLogin(UserLogin userLogin) {
         this.userLogin = userLogin;
     }
 
-/*用户登录，查询用户*/
+    /*用户登录，查询用户*/
+    @ResponseBody
     @RequestMapping("/findUser")
-    public String find(String emailAddress, String password){
+    public String find(String emailAddress, String password) {
         boolean ans = userLogin.findUser(emailAddress, password);
-        if(ans){
+        if (ans) {
             return "ok".toString();
-        }else{
+        } else {
             return "not ok".toString();
         }
     }
